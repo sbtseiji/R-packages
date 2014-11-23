56dccec503da61e3b698f14b9467e447f8a268e8
SHIBATA Seiji <sbtseiji@users.noreply.github.com>
2014-11-23 19:46:12 +0900

    first commit

---
+++ gm_cov_select.R
@@ -0,0 +1,32 @@
+cov_select<-function(model=x, cmat=y, n.obs=0, AIC=Inf){
+  require(ggm)
+
+  # 偏相関行列を作成
+  pmat<- (-1)*solve(cmat) / sqrt(diag(solve(cmat)) %*% t(sqrt(diag(solve(cmat)))))
+  diag(pmat)<- 1
+
+  # 相関係数を絶対値に変換
+  amat<-abs(cmat)
+
+  # モデルの係数が0の箇所を無限大に設定
+  amat[which(model==0)]<-Inf
+
+  # 偏相関の絶対値が最小の要素を0にした修正モデルを作成
+  model_post<-model
+  model_post[which(amat==min(amat))]<-0
+
+  # モデルのフィットとAICの算出
+  f<-fitConGraph(model_post,cmat,n.obs)
+  AIC_post<-f$dev-2*f$df
+
+  # モデルの適合度が最大になるまで反復
+  if (AIC_post<AIC){
+    Recall(model_post,f$Shat,n.obs,AIC_post)
+  }
+  # 最終的に得られたモデルを描画 & 偏相関行列を表示
+  else{
+    drawGraph(model,adjust=FALSE)
+    pmat[which(model==0)]<-0
+    (pmat)
+  }
+}
