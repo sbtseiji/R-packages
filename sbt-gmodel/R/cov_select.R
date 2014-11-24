a57f1b42551c90cafd8af76fd4603165ca7c5728
SHIBATA Seiji <contact@sbtseiji.net>
2014-11-24 23:48:09 +0900

    出力の対角成分を1に

--- sbt-gmodel/R/cov_select.R
+++ sbt-gmodel/R/cov_select.R
@@ -29,7 +29,8 @@ cov_select<-function(model=x, cov=y, n.obs=0, AIC=Inf){
   # 最終的に得られたモデルを描画 & 偏相関行列を表示
   else{
     drawGraph(model,adjust=FALSE)
-    pmat[which(model==0)]<-0
+    pmat[which(model==0)]<-0 # 指数表記になるのを防ぐ
+    diag(pmat)<-1 # 対角成分を1に
     (pmat)
   }
 }
