fb53cecdcfcea3cd81c7e905fe6e1d47f95aa2f7
SHIBATA Seiji <contact@sbtseiji.net>
2014-11-28 21:14:40 +0900

    fitConGraphに使用する共分散が誤っていたのを修正

--- R/cov_select.R
+++ R/cov_select.R
@@ -1,15 +1,12 @@
-cov_select<-function(model=x, cov=y, n.obs=0, AIC=Inf){
-  require(ggm)
-
-  #' 相関行列を作成
-  cmat<-cov / sqrt(diag(cov) %*% t(diag(cov)))
-
+cov_select<-function(model=x, cov=y, n.obs=0, AIC=0, cov_orig=NULL){
+  if(is.null(cov_orig)){cov_orig<-cov}
+  print (AIC)
   #' 偏相関行列を作成
-  pmat<- (-1)*solve(cmat) / sqrt(diag(solve(cmat)) %*% t(diag(solve(cmat))))
+  pmat<- (-1)*solve(cov) / sqrt(diag(solve(cov)) %*% t(diag(solve(cov))))
   diag(pmat)<- 1
 
-  #' 相関係数を絶対値に変換
-  amat<-abs(cmat)
+  #' 偏相関係数を絶対値に変換
+  amat<-abs(pmat)
 
   #' モデルの係数が0の箇所を無限大に設定
   amat[which(model==0)]<-Inf
@@ -19,12 +16,12 @@ cov_select<-function(model=x, cov=y, n.obs=0, AIC=Inf){
   model_post[which(amat==min(amat))]<-0
 
   #' モデルのフィットとAICの算出
-  f<-fitConGraph(model_post,cov,n.obs)
+  f<-fitConGraph(model_post,cov_orig,n.obs)
   AIC_post<-f$dev-2*f$df
 
   #' モデルの適合度が最大になるまで反復
   if (AIC_post<AIC){
-    Recall(model_post,f$Shat,n.obs,AIC_post)
+    Recall(model_post,f$Shat,n.obs,AIC=AIC_post,cov_orig=cov)
   }
   #' 最終的に得られたモデルを描画 & 偏相関行列を表示
   else{
